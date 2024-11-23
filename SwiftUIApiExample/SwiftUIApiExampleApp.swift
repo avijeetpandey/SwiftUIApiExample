60a50d93a2cc8acb3846c76d254aab58235bd958
Avijeet <avijeetpandey25@gmail.com>
2024-11-24 01:28:52 +0530

    Initial Commit

---
+++ SwiftUIApiExample/SwiftUIApiExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIApiExampleApp.swift
+//  SwiftUIApiExample
+//
+//  Created by Avijeet Pandey on 24/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIApiExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
